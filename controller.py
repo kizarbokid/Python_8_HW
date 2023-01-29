f000bd0937654a39e3acd03ba808be1c1fcf6435
YakovkinVD <dikobrazzzik709@gmail.com>
2023-01-29 23:14:59 +0900

    start to build architecture

---
+++ controller.py
@@ -0,0 +1,27 @@
+import model
+import view
+
+
+
+def start():
+    user_input=0
+    while user_input!=6:
+        match user_input:
+            case 1: #Подключиться к файлу
+                
+                pass
+            case 2: #Начать урок
+                
+                pass
+            case 3: #Посмотреть поименный список класса
+                
+                pass
+            case 4: #Посмотреть оценки по предмету определенного класса
+                
+                pass
+            case 5: #Записать в файл
+                
+                pass
+            case 6: #Выход
+                
+                pass
